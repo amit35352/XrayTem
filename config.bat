f13dd284f608afe8267fff182c092d8837766634
unknown <amit35352@gmail.com>
2020-06-12 22:03:25 +0530

    committed

---
+++ config.bat
@@ -0,0 +1,5 @@
+set teIssue=ISSUE-100
+set jfile=@C:\Users\DEII\.jenkins\workspace\Demo_Proj\Robot_Xray\json\testex.json
+set jupdate=@C:\Users\DEII\.jenkins\workspace\Demo_Proj\Robot_Xray\json\example.json
+set rootURL=http://localhost:8000
+set token=c3AudGVzdGluZzIwMTg6c3B0ZXN0aW5nMg==
